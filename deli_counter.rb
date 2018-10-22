a41fdf4778101dca3cc00916691ae299cab50ad0
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 21:46:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(deli)
+  output = "The line is currently"
+  if deli.size == 0
+    output += " empty."
+  else
+    output += ":"
+    deli.each_with_index do |name, index|
+      output += " #{index + 1}. #{name}"
+    end
+  end
+  puts output
+end
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving(deli)
+  if deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli.shift}."
+    deli
+  end
+end
